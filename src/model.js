cc16387937dc6a5ec97acc29aea5b0e242b3e66b
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-23 12:02:56 +0100

    Finish wireframe

--- src/model.js
+++ src/model.js
@@ -1,13 +1,8 @@
-//Todo: import control
-import { runInContext } from "lodash";
-import Control from "./control";
-//Todo: export model class to control
 export default class Model {
-//Todo: Within class create array of obj
-constructor() {
-  this.task = [
-    {string: 'run', bool: false, number:1},
-    {string: 'exercise', bool: false, number:2}
-];
-}
+  constructor() {
+    this.tasks = [
+      { string: 'run', bool: false, number: 1 },
+      { string: 'exercise', bool: false, number: 2 },
+    ];
+  }
 }
--- src/view.js
+++ src/view.js
@@ -1,17 +1,37 @@
-//Todo: import from control
+export default class View {
+  constructor() {
+    this.list = document.querySelector('.items');
+    this.fragment = new DocumentFragment();
+  }
 
-//Todo: create and export view class 
-//Todo: add populateList to consructor
-//Todo: get ul from index
-//Todo: append populatelist to ul
+  createElements(tasks) {
+    tasks.forEach((currentItem) => {
+      // this.list.innerHTML = '';
+      this.div = document.createElement('div');
+      this.div.classList.add('row');
+      this.checkbox = document.createElement('input');
+      this.checkbox.setAttribute('type', 'checkbox');
+      this.checkbox.classList.add('no-fluid');
+      this.input = document.createElement('input');
+      this.input.setAttribute('type', 'text');
+      this.input.setAttribute('readonly', true);
+      this.input.setAttribute('draggable', true);
+      this.input.value = `${currentItem.string}`;
+      this.input.style.cssText = 'border: none';
+      this.more = document.createElement('span');
+      this.more.classList = 'more';
+      this.more.innerHTML = '&#65049;';
+      this.bin = document.createElement('span');
+      this.bin.innerHTML = '&#128465;';
+      this.bin.classList = 'bin';
+      this.bin.classList.add('no-fluid');
+      this.div.append(this.checkbox, this.input, this.more, this.bin);
+      this.fragment.appendChild(this.div);
+    });
+    this.list.append(this.fragment);
+  }
 
-//Todo: create metho
-
-//Todo: create drag and dop method
-
-//Todo: method to put dach when checkbox is clicked 
-
-//Todo: method to erase clicked
-
-
-
+  display(tasks) {
+    this.createElements(tasks);
+  }
+}

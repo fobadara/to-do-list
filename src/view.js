c9409dd08a7985f6d5fd6e683ca2242bbf9b4a27
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-23 17:26:17 +0100

    Update view

--- src/view.js
+++ src/view.js
@@ -12,22 +12,41 @@
 //Todo: method to put dach when checkbox is clicked 
 
 //Todo: method to erase clicked
-
-import './style.css';
-import View from './view.js';
-import Model from './model.js';
-
-class Control {
-  constructor(view) {
-    this.model = new Model();
-    this.view = view;
+export default class View {
+  constructor() {
+    this.list = document.querySelector('.items');
+    this.fragment = new DocumentFragment();
   }
 
-  init() {
-    // initialize model update
-    this.view.display(this.model.tasks);
+  createElements(tasks) {
+    tasks.forEach((currentItem) => {
+      // this.list.innerHTML = '';
+      this.div = document.createElement('div');
+      this.div.classList.add('row');
+      this.checkbox = document.createElement('input');
+      this.checkbox.setAttribute('type', 'checkbox');
+      this.checkbox.classList.add('no-fluid');
+      this.input = document.createElement('input');
+      this.input.style.cssText = 'border: none;';
+      this.input.setAttribute('type', 'text');
+      this.input.setAttribute('readonly', true);
+      this.input.setAttribute('draggable', true);
+      this.input.value = `${currentItem.string}`;
+      this.input.style.cssText = 'border: none';
+      this.more = document.createElement('span');
+      this.more.classList = 'more';
+      this.more.innerHTML = '&#65049;';
+      this.bin = document.createElement('span');
+      this.bin.innerHTML = '&#128465;';
+      this.bin.classList = 'bin';
+      this.bin.classList.add('no-fluid');
+      this.div.append(this.checkbox, this.input, this.more, this.bin);
+      this.fragment.appendChild(this.div);
+    });
+    this.list.append(this.fragment);
+  }
+
+  display(tasks) {
+    this.createElements(tasks);
   }
 }
-
-const control = new Control(new View());
-control.init();

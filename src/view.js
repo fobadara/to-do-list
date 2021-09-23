7dc103338a2b98019133b0a0929166b9150aae59
fobadara <62938735+fobadara@users.noreply.github.com>
2021-09-23 12:12:17 +0100

    Update view

--- src/view.js
+++ src/view.js
@@ -13,5 +13,22 @@
 
 //Todo: method to erase clicked
 
+import './style.css';
+import View from './view.js';
+import Model from './model.js';
 
+class Control {
+  constructor(view) {
+    this.model = new Model();
+    this.view = view;
+  }
+
+  init() {
+    // initialize model update
+    this.view.display(this.model.tasks);
+  }
+}
+
+const control = new Control(new View());
+control.init();
 
